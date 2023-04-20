1969642214d62420b84fb7efd5394e38ea778dd9
Vovchik <vova2000ra@mail.ru>
2023-04-21 04:00:30 +0600

    section

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,28 +1,49 @@
-var swiperImage = new Swiper(".review__slider", {
+var swiperReview = new Swiper(".review__slider", {
     navigation: {
         nextEl: ".review-button-next",
         prevEl: ".review-button-prev",
+    },
+    pagination: {
         el: '.review-pagination',
         clickable: true,
     },
+    slidesPerView: 3,
     spaceBetween: 30,
-    slidesPerView: 'auto',
     autoplay: {
         delay: 5000,
         disableOnInteraction: false,
     },
 });
-var swiperImage = new Swiper(".letter__slider", {
+var swiperLetter = new Swiper(".letter__slider", {
     navigation: {
         nextEl: ".letter-button-next",
         prevEl: ".letter-button-prev",
+    },
+    pagination: {
         el: '.letter-pagination',
         clickable: true,
     },
+    slidesPerView: 4,
     spaceBetween: 30,
-    slidesPerView: 'auto',
     autoplay: {
-        delay: 5000,
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+});
+var swiperTrust = new Swiper(".trust__slider", {
+    navigation: {
+        nextEl: ".trust-button-next",
+        prevEl: ".trust-button-prev",
+    },
+    pagination: {
+        el: '.trust-pagination',
+        clickable: true,
+    },
+    slidesPerView: 6,
+    spaceBetween: 30,
+    grabCursor: true,
+    autoplay: {
+        delay: 4000,
         disableOnInteraction: false,
     },
 });
